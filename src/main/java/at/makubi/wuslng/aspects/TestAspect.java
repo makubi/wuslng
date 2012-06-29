8c48c420223efb8197fcdad1a2bdb762fcf5e8c6
Mathias Kub <git@makubi.at>
2012-06-29 12:03:22 +0200

    Reauthentication ueber Around-Aspect

--- src/main/java/at/makubi/wuslng/aspects/TestAspect.java
+++ src/main/java/at/makubi/wuslng/aspects/TestAspect.java
@@ -3,12 +3,39 @@ package at.makubi.wuslng.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import at.makubi.wuslng.untiswsclient.UntisWsClient;
+
+import com.googlecode.jsonrpc4j.JsonRpcClientException;
 
 @Aspect
 public class TestAspect {
 	
+	private UntisWsClient untisWsClient;
+	
+	@Autowired
+	public TestAspect(UntisWsClient untisWsClient) {
+		this.untisWsClient = untisWsClient;
+	}
+	
 	@Around("within(at.makubi.wuslng.services..*)")
 	public Object aroundServiceCalls(ProceedingJoinPoint pjp) throws Throwable {
-		return pjp.proceed();
+		try {
+			return pjp.proceed();
+		}
+		catch (JsonRpcClientException e) {
+			handleServiceException(e);
+			return pjp.proceed();
+		}
+	}
+
+	private void handleServiceException(JsonRpcClientException e) {
+		if(e.getCode() == -8520) {
+			if(e.getMessage().equals("not authenticated")) {
+				System.out.println("Reauthenticating");
+				untisWsClient.authenticate();
+			}
+		}
 	}
 }
